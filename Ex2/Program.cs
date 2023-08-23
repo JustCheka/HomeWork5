48a8d0a4de6c353e4fe8b88f3e6f9e6f31c66933
JustCheka <cheka201@yandxe.ru>
2023-08-24 00:38:37 +0300

    Ex 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿void InitArr(int[] arr){
+    for (int i = 0; i < arr.Length; i++){
+        arr[i] = new Random().Next(0, 1000);
+    }
+}
+
+void PrintArr(int[] arr){
+    foreach (var el in arr){
+        Console.Write(el + " ");
+    }
+    Console.WriteLine();
+}
+
+int SumOfNum(int[] arr){
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2){
+        sum += arr[i];
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите количество элемнтов в массиве");
+int n = int.Parse(Console.ReadLine());
+
+int[] arr = new int[n];
+
+InitArr(arr);
+PrintArr(arr);
+Console.WriteLine(SumOfNum(arr));
